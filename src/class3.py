f3ba87bdec866e60a062869ba5a2d10599134a14
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2019-10-04 21:43:10 -0400

    Added test class3

---
+++ src/class3.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from logml import *
+
+# Example of using 'Clasification: 3 inputs' dataset
+
+
+def to_class(c):
+    if c < -3:
+        return 'low'
+    if c < 3:
+        return 'mid'
+    return 'high'
+
+
+# Create dataset
+def create_dataset():
+    # Number of samples
+    num = 1000
+    # Inputs: x1, x2, x3
+    x1 = np.random.normal(0, 1, num)
+    x2 = np.random.normal(2, 3, num)
+    x3 = np.random.normal(-3, 5, num)
+    # Noise
+    n = np.random.normal(0, 1, num)
+    # Output
+    y = 3. * x1 - 1. * x2 + 0.5 * x3 + 0.1 * n
+    # Categorical output
+    y_str = np.array([to_class(c) for c in y])
+    # Create dataFrame
+    df = pd.DataFrame({'x1': x1, 'x2': x2, 'x3': x3, 'y': y_str})
+    df.to_csv('class3c.csv', index=False)
+
+
+ml = LogMl()
+ml()
+print("Done!")
