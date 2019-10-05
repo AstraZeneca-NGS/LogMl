27956e626a47980aebd6a48d1fc7d6e4cb968f34
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2019-10-04 20:20:10 -0400

    Added more models

--- config/models_search/z.py
+++ config/models_search/z.py
@@ -16,5 +16,7 @@ with open('z.yaml') as yaml_file:
         s = camel_to_snake(n)
         print(f"{n}:{s}\n{y[n]}")
         file_name = f"{s}.yaml"
+        d = dict()
+        d[n] = y[n]
         with open(file_name, 'w') as out:
-            yaml.dump(data, out, default_flow_style=False)
+            yaml.dump(d, out, default_flow_style=False)
