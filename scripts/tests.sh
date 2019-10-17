ca00bd6c3999c2a3441d122e89a808ba30ed704d
Pablo Cnigolani <pablo.e.cingolani@gmail.com>
2019-10-17 16:57:10 -0400

    Project updated

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -7,14 +7,35 @@ export PS1=""
 
 source ./bin/activate
 
+test_integration="${1:-false}"
+
+# Only perform one test
 TEST_NAME=""
 # TEST_NAME="TestLogMl.test_dataset_transform_002"
 
+echo
+echo
 if [ -z "$TEST_NAME" ]; then
-    time coverage run src/tests.py -v --failfast 2>&1 | tee tests.out
+    echo "Unit tests: All "
+    time coverage run src/tests.py -v --failfast 2>&1 | tee tests.unit.out
 else
-    time coverage run src/tests.py -v --failfast "$TEST_NAME" 2>&1 | tee tests.out
+    echo "Unit test: '$TEST_NAME' "
+    time coverage run src/tests.py -v --failfast "$TEST_NAME" 2>&1 | tee tests.unit.out
 fi
 
-coverage report -m --fail-under=60 --omit='*lib/*' 2>&1 | tee -a tests.out
+coverage report -m --fail-under=60 --omit='*lib/*' 2>&1 | tee -a tests.unit.out
+
+# Should we do integration testing?
+if [ "$test_integration" eq 'false']; do
+  exit
+fi
+
+echo
+echo
+echo "Integration tests"
+
+./src/tests_integration.py -v --failfast 2>&1 | tee tests.integration.out
+
+echo
+echo
 echo "Done: All tests passed"
