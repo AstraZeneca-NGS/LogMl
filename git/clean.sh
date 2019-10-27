def59f50640786d30dcd4826618a7436dc35b5aa
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2019-10-26 20:10:28 -0400

    Project updated

--- git/clean.sh
+++ git/clean.sh
@@ -3,6 +3,7 @@
 # Cleanup files from local file system befor commit to repo
 rm -rvf tests/tmp/
 
+rm -vf `find . -iname "*.pyc" -type f`
 rm -vf `find . -iname "*.pkl" -type f`
 rm -vf `find . -iname "*.stdout" -type f`
 rm -vf `find . -iname "*.stderr" -type f`
