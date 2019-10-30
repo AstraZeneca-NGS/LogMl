eecdb4f118e50dc402969465255fdba76494c79e
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2019-10-30 14:02:24 -0400

    Project updated

--- git/clean.sh
+++ git/clean.sh
@@ -8,3 +8,5 @@ rm -vf `find . -iname "*.pkl" -type f`
 rm -vf `find . -iname "*.stdout" -type f`
 rm -vf `find . -iname "*.stderr" -type f`
 rm -vf tests/integration/model/*
+rm -vf tests/integration/data/*.dot
+rm -vf tests/integration/data/*.png
