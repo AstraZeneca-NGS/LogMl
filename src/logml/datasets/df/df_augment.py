2094998e4edd1b67077f127875e68ee793a458f0
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2020-01-01 20:09:25 -0300

    Added NMF

--- src/logml/datasets/df/df_augment.py
+++ src/logml/datasets/df/df_augment.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 from collections import namedtuple
-from sklearn.decomposition import PCA
+from sklearn.decomposition import NMF, PCA
 
 from ...core.config import CONFIG_DATASET_AUGMENT
 from ...core.log import MlLog
@@ -39,10 +39,21 @@ class DfAugment(MlLog):
             return self.df
         cols = "', '".join([c for c in self.df.columns])
         self._debug(f"Augment dataframe: Start. Fields ({len(self.df.columns)}): ['{cols}']")
+        self._nmf()
         self._pca()
         cols = "', '".join([c for c in self.df.columns])
         self._debug(f"Augment dataframe: End. Fields ({len(self.df.columns)}): ['{cols}']")
         return self.df
+
+    def _nmf(self):
+        self.nmf_augment = DfAugmentNmf(self.df, self.config, self.outputs, self.model_type)
+        ret = self.nmf_augment()
+        if ret is None:
+            self._debug("Augment dataframe: Could not do NMF")
+            return False
+        else:
+            self.df = self.df.join(ret)
+            return True
 
     def _pca(self):
         self.pca_augment = DfAugmentPca(self.df, self.config, self.outputs, self.model_type)
@@ -53,6 +64,26 @@ class DfAugment(MlLog):
         else:
             self.df = self.df.join(ret)
             return True
+
+
+class DfAugmentNmf(CountAndFields):
+    ''' Augment dataset by adding Non-negative martix factorization '''
+
+    def __init__(self, df, config, outputs, model_type, set_config=True):
+        super().__init__(df, config, CONFIG_DATASET_AUGMENT, 'nmf', df.columns, outputs)
+        self.sk_nmf_by_name = dict()
+
+    def calc(self, nf, x):
+        """Calculate 'num' NMFs using 'fields' from dataframe
+        Returns: A dataframe of NMFs (None on failure)
+        """
+        self._debug(f"Calculating NMF: Start, name={nf.name}, num={nf.number}, fields:{nf.fields}")
+        nmf = NMF(n_components=nf.number)
+        nmf.fit(x)
+        self.sk_nmf_by_name[nf.name] = nmf
+        xnmf = nmf.transform(x)
+        self._debug(f"Calculating NMF: End")
+        return xnmf
 
 
 class DfAugmentPca(CountAndFields):
