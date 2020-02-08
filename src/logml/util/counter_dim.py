b9a0001b01a4bdc4b9e341c0d1683d60f75076cf
Pablo Cnigolani <pablo.e.cingolani@gmail.com>
2020-02-08 16:02:43 -0500

    Project updated

--- src/logml/util/counter_dim.py
+++ src/logml/util/counter_dim.py
@@ -2,21 +2,11 @@
 class CounterDim:
     """
     Counts in 'dims' dimentions up to 'max_count' in each dimension.
-    If only_increasing is true, numbers in are increasing (i.e. number
-    combinations are not repeated)
     """
-    def __init__(self, max_count, dims, only_increasing=False):
+    def __init__(self, max_count, dims):
         self.max_count = max_count
         self.dims = dims
-        self.only_increasing = only_increasing
         self.counters = np.zeros(self.dims, dtype=int)
-
-    def is_increasing(self):
-        " Are counters increasing "
-        for i in range(1, self.dims):
-            if self.counters[i - 1] <= self.counters[i]:
-                return False
-        return True
 
     def __iter__(self):
         " Initilize iteration "
@@ -25,11 +15,8 @@ class CounterDim:
         return self
 
     def __next__(self):
-        """ Next (valid) counter """
-        while True:
-            ret = self._next_counter()
-            if not self.only_increasing or self.is_increasing():
-                return ret
+        """ Next array of numbers """
+        return self._next_counter()
 
     def _next_counter(self):
         """ Get next valid counter """
@@ -42,3 +29,27 @@ class CounterDim:
 
     def __repr__(self):
         return str(self.counters)
+
+
+class CounterDimIncreasing(CounterDim):
+    """
+    Counts in 'dims' dimentions up to 'max_count' in each dimension, such that
+    numbers in the array are in increasing order (i.e. number combinations are
+    not repeated)
+    """
+    def __init__(self, max_count, dims):
+        super().__init__(self, max_count, dims)
+
+    def is_increasing(self):
+        " Are numbers in the array in increasing order? "
+        for i in range(1, self.dims):
+            if self.counters[i - 1] <= self.counters[i]:
+                return False
+        return True
+
+    def __next__(self):
+        """ Next (valid) counter """
+        while True:
+            ret = self._next_counter()
+            if self.is_increasing():
+                return ret
